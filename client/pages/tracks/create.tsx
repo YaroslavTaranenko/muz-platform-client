0e3eadb762db6d7c097f557f111fe988786ffcab
Ярослав Тараненко <taranenkojaroslav@gmail.com>
2021-10-28 21:10:14 +0600

    StepWrapper

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,5 +1,6 @@
-import { Button, Grid } from '@mui/material'
+import { Button, Grid, TextField } from '@mui/material'
 import { useState } from 'react'
+import FileUpload from '../../components/FileUpload'
 import StepWrapper from '../../components/StepWrapper'
 import MainLayout from '../../layouts/MainLayout'
 
@@ -12,12 +13,42 @@ const Create = () => {
     setActiveStep((prev) => prev - 1)
   }
 
+  const [picture, setPicture] = useState(null)
+  const [audio, setAudio] = useState(null)
+
   return (
     <MainLayout>
       <StepWrapper activeStep={activeStep}>
-        {activeStep === 0 && <h1>Step 1</h1>}
-        {activeStep === 1 && <h1>Step 2</h1>}
-        {activeStep === 2 && <h1>Step 3</h1>}
+        {activeStep === 0 && (
+          <Grid container direction="column" style={{ padding: 20 }}>
+            <TextField
+              style={{ marginTop: 10 }}
+              label="Название песни"
+            ></TextField>
+            <TextField
+              style={{ marginTop: 10 }}
+              label="Имя исполнителя"
+            ></TextField>
+            <TextField
+              style={{ marginTop: 10 }}
+              label="Текст песни"
+              multiline
+              rows={3}
+            ></TextField>
+          </Grid>
+        )}
+        {activeStep === 1 && (
+          <FileUpload setFile={setPicture} accept="image/*">
+            <Button>Загрузите обложку</Button>
+            <div>File: {picture?.name}</div>
+          </FileUpload>
+        )}
+        {activeStep === 2 && (
+          <FileUpload setFile={setAudio} accept="audio/*">
+            <Button>Загрузите аудио трэк</Button>
+            <div>File: {audio?.name}</div>
+          </FileUpload>
+        )}
       </StepWrapper>
       <Grid container justifyContent="space-between">
         <Button disabled={activeStep === 0} onClick={back}>
